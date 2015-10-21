b55c90d19adb6d308b045f73c71e90b22e5686c6
VaniaStoicheva <cecostoi4ev@abv.bg>
2015-10-21 23:06:48 +0300

    refactoring for BookShop_Loginform

--- include/template_function.php
+++ include/template_function.php
@@ -1,6 +1,9 @@
 <?php
 
-function render($data,$name){
- 
-    include $name;
+function render($data,$path){
+ if(file_exists($path)){
+    include $path;
+}else{
+    echo '<p>Некоректен път до файла</p>';
+}
 }
